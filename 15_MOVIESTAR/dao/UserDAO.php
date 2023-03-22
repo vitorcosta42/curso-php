80a784129265c89417eda72748826f8c352b377d
Vitor <vitorcostadev@gmail.com>
2023-03-22 11:58:17 -0300

    register user implementation

---
+++ 15_MOVIESTAR/dao/UserDAO.php
@@ -0,0 +1,94 @@
+<?php
+require_once("models/User.php");
+require_once("models/Message.php");
+
+class UserDAO implements UserDAOInterface
+{
+    private $conn;
+    private $url;
+    private $message;
+    public function __construct(PDO $conn, $url)
+    {
+        $this->conn = $conn;
+        $this->url = $url;
+        $this->message=new Message($url);
+    }
+
+    public function buildUser($data)
+    {
+        $user = new User();
+        $user->id = $data["id"];
+        $user->name = $data["name"];
+        $user->lastname = $data["lastname"];
+        $user->email = $data["email"];
+        $user->password = $data["password"];
+        $user->image = $data["image"];
+        $user->bio = $data["bio"];
+        $user->token = $data["token"];
+        return $user;
+    }
+    public function create(
+        User $user,
+        $authUser = false
+    ) {
+        $stmt = $this->conn->prepare("INSERT INTO users (name,lastname,email,password,token)
+         VALUES (
+            :name,:lastname,:email,:password,:token
+        )");
+        $stmt->bindParam(":name", $user->name);
+        $stmt->bindParam(":lastname", $user->lastname);
+        $stmt->bindParam(":email", $user->email);
+        $stmt->bindParam(":password", $user->password);
+        $stmt->bindParam(":token", $user->token);
+        $stmt->execute();
+        
+        // Autenticar usuário, caso auth seja true
+            if($authUser) { 
+                $this->setTokenToSession($user->token);
+            }
+    }
+    public function update(User $user)
+    {
+    }
+    public function findByToken($token)
+    {
+    }
+    public function verifyToken($protected = false)
+    {
+    }
+    public function setTokenToSession($token, $redirect = true)
+    {
+        // Salvar token na sessão
+        $_SESSION["token"] = $token;
+        if($redirect) { 
+            // Redireciona para o perfil do usuário 
+            $this->message->setMessage("Seja bem-vindo!","success","editprofile.php");
+        }
+    }
+    public function authenticateUser($email, $password)
+    {
+    }
+    public function findByEmail($email)
+    {
+        if ($email != "") {
+            $stmt = $this->conn->prepare("SELECT * FROM users WHERE email = :email");
+            $stmt->bindParam(":email", $email);
+            $stmt->execute();
+            if ($stmt->rowCount() > 0) {
+                $data = $stmt->fetch();
+                $user = $this->buildUser($data);
+                return $user;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    public function findById($id)
+    {
+    }
+    public function changePassword(User $user)
+    {
+    }
+}
---
+++ 15_MOVIESTAR/templates/footer.php
@@ -0,0 +1,36 @@
+<footer id="footer">
+        <div id="social-container">
+            <ul>
+                <li>
+                    <a href="#">
+                        <i class="fab fa-facebook-square"></i>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fab fa-youtube"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div id="footer-links-container">
+            <ul>
+                <li><a href="#">Adicionar filme</a></li>
+                <li><a href="#">Adicionar crítica</a></li>
+                <li><a href="#">Entrar / Registrar</a></li>
+            </ul>
+        </div>
+        <p> &copy; 2023 Hora de Codar</p>
+    </footer>
+    <!-- BOOTSTRAP JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.3/js/bootstrap.js" integrity="sha512-L6XANV6sOsx9N9c787eDN1pjB2Pzautd3xDgn4cMKuoleHSuCJi5pCDGPCtwE3Bd4A1Olnr0k0aQXbczYzg+wg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+
+</html>
