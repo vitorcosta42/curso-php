9adc8c73cde28460422de490cbfaa2425aaf16e2
Vitor <vitorcostadev@gmail.com>
2023-03-22 16:01:19 -0300

    editprofile implementations

--- 15_MOVIESTAR/models/User.php
+++ 15_MOVIESTAR/models/User.php
@@ -1,7 +1,7 @@
 <?php
 
-class User
-{
+  class User {
+
     public $id;
     public $name;
     public $lastname;
@@ -11,27 +11,36 @@ class User
     public $bio;
     public $token;
 
-    public function generateToken() { 
-        return bin2hex(random_bytes(50));
+    public function getFullName($user) {
+      return $user->name . " " . $user->lastname;
     }
-    public function generatePassword($password){
-        return password_hash($password,PASSWORD_DEFAULT);
-    }
-}
 
-interface UserDAOInterface
-{
+    public function generateToken() {
+      return bin2hex(random_bytes(50));
+    }
+    
+    public function generatePassword($password) {
+      return password_hash($password, PASSWORD_DEFAULT);
+    }
+
+    public function imageGenerateName() {
+      return bin2hex(random_bytes(60)) . ".jpg";
+    }
+
+  }
+
+  interface UserDAOInterface {
+
     public function buildUser($data);
-    public function create(
-        User $user,
-        $authUser = false
-    );
-    public function update(User $user);
-    public function findByToken($token);
+    public function create(User $user, $authUser = false);
+    public function update(User $user, $redirect = true);
     public function verifyToken($protected = false);
-    public function setTokenToSession($token,$redirect=true);
-    public function authenticateUser($email,$password);
+    public function setTokenToSession($token, $redirect = true);
+    public function authenticateUser($email, $password);
     public function findByEmail($email);
     public function findById($id);
+    public function findByToken($token);
+    public function destroyToken();
     public function changePassword(User $user);
-}
+
+  }
